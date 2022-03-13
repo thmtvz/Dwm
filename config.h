0a7086a51eb970d81953a17b56e5f7cc1ca6db7e
Thiago Vaz <thiago@martinsvaz.com>
2022-03-13 19:34:24 -0300

    update

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceCodePro-Regular:size=10" };
+static const char *fonts[]          = { "SourceCodePro-Regular:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "SourceCodePro-Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -41,7 +41,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "<>",       NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -60,26 +60,32 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", "black", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *brave[]  = { "firefox", NULL };
+static const char *firefox[]  = { "firefox", NULL };
+static const char *qute[]  = { "qutebrowser", NULL };
 static const char *pulsemixer[]  = { "st","-e", "pulsemixer" };
-static const char *virt_manager[]  = { "virt-manager", NULL };
-static const char *editor[]  = { "emacsclient", "--create-frame", NULL };
+static const char *editor[]  = { "emacsclient", "-c", NULL };
 static const char *nnn[]  = { "st", "-e", "nnn" };
 static const char *tor[]  = { "torbrowser-launcher", NULL };
 static const char *qbittorrent[]  = { "qbittorrent", NULL };
 static const char *ncmpcpp[] = { "st", "ncmpcpp", NULL };
+static const char *newsboat[] = { "st", "newsboat", NULL };
+static const char *top[] = { "st", "top", NULL };
+static const char *nmtui[] = { "st", "nmtui", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          {.v = brave } },
-	{ MODKEY,                       XK_v,      spawn,          {.v = virt_manager } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = firefox } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = qute } },
 	{ MODKEY,                       XK_z,      spawn,          {.v = nnn } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = editor } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = tor } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = qbittorrent } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = ncmpcpp } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = newsboat } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = top } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = nmtui } },
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("redshift -x")  },
 	{ MODKEY,                       XK_o,      spawn,          SHCMD("redshift -O 4000K")  },
 	{ MODKEY|ShiftMask,             XK_F9,     spawn,          SHCMD("mpc prev")  },
@@ -110,10 +116,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY,                       XK_F1,     spawn,          SHCMD("systemctl suspend")  },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = gappx } },
+	{ MODKEY,                       XK_F1,     spawn,          SHCMD("")  },
 	{ MODKEY,                       XK_F2,     spawn,          SHCMD("slock")  },
-	{ MODKEY,                       XK_F3,     spawn,          SHCMD("reboot")  },
-	{ MODKEY,                       XK_F4,     spawn,          SHCMD("shutdown now")  },
+	{ MODKEY,                       XK_F3,     spawn,          SHCMD("")  },
+	{ MODKEY,                       XK_F4,     spawn,          SHCMD("")  },
 	{ MODKEY,                       XK_F5,     spawn,          SHCMD("xbacklight -set 0")  },
 	{ MODKEY,                       XK_F6,     spawn,          SHCMD("xbacklight -dec 5")  },
 	{ MODKEY,                       XK_F7,     spawn,          SHCMD("xbacklight -inc 5")  },
